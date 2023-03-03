55cae6480ae42bece6edcdb483b7acfa562a9a22
raghu0056 <raghumani0056@gmail.com>
2023-03-03 22:35:35 +0530

    multiplication added

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         int b=20;
         int sum=a+b;
         int sub=a-b;
+        int mul=a+b;
         System.out.println("The addition is " +sum);
         System.out.println("The addition is " +sub);
-   }
+        System.out.println("The addition is " +mul}
 }
+
+
