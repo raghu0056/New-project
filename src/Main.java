201fc7b5adcca5f59e3867e1ac47131255fbb127
raghu0056 <raghumani0056@gmail.com>
2023-03-03 21:21:15 +0530

    Calculator  programm

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int sub=a-b;
+        System.out.println("The addition is " +sum);
+        System.out.println("The addition is " +sub);
+   }
+}
