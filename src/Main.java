f6d7febc000a7d354bd7b58ae3b35967cf802999
raghu0056 <raghumani0056@gmail.com>
2023-03-07 20:57:59 +0530

    Calculation programm using switchcase

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
-        int a=10;
-        int b=20;
-        int sum=a+b;
-        int sub=a-b;
-        int mul=a+b;
-        System.out.println("The addition is " +sum);
-        System.out.println("The addition is " +sub);
-        System.out.println("The addition is " +mul}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double a = sc.nextDouble();
+        System.out.println("Enter the Second number");
+        double b = sc.nextDouble();
+        System.out.println("Enter your choice \n 1) Addition \n 2) Subtraction \n 3) Multiplication \n 4) Division \n 5) Remainder");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                double add = a + b;
+                System.out.println("The subtraction is  " + add);
+                break;
+            case 2:
+                double sub = a - b;
+                System.out.println("The subtraction is  " + sub);
+                break;
+            case 3:
+                double mul = a * b;
+                System.out.println("The subtraction is  " + mul);
+                break;
+            case 4:
+                double div = a / b;
+                System.out.println("The subtraction is  " + div);
+                break;
+            case 5:
+                double mod = a % b;
+                System.out.println("The subtraction is  " + mod);
+                break;
+            default:
+                System.out.println("wrong choice not in the list ");
+        }
+            sc.close();
+
+    }
 }
-
-
